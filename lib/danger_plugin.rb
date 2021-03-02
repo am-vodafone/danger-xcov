a585685356a30b264f585c1f17f56f679f6f2950
abdelrahman-ahmed5 <64151606+abdelrahman-ahmed5@users.noreply.github.com>
2021-03-02 18:12:19 +0200

    Update danger_plugin.rb

--- lib/danger_plugin.rb
+++ lib/danger_plugin.rb
@@ -97,8 +97,8 @@ module Danger
 
     # Filters the files that haven't been modified in the current PR
     def process_report(report)
-      file_names = @dangerfile.git.modified_files.map { |file| File.expand_path(file) }
-      file_names += @dangerfile.git.added_files.map { |file| File.expand_path(file) }
+      #file_names = @dangerfile.git.modified_files.map { |file| File.expand_path(file) }
+      file_names = @dangerfile.git.added_files.map { |file| File.expand_path(file) }
       report.targets.each do |target|
         target.files = target.files.select { |file| file_names.include?(file.location) }
       end
